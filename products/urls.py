7457ecc4e93d42391a526f2de4fffe585718f3aa
Shreya Karn <skarn6469@gmail.com>
2024-09-25 17:03:35 +0545

    Add Product added

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('view-products/', views.product, name='product'),
     path('product-details/<int:id>/', views.single_product, name='productdetails'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('add-product/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from products.forms import ProductForm
 from products.models import Product
 
 # Create your views here.
@@ -10,7 +11,7 @@ def product(request):
 
 
 def single_product(request,id):
-    single_products = Product.objects.get(id=id)
+    single_products = Product.objects.get(id=id).first()
     data = {
         'products':single_products
     }
@@ -18,4 +19,28 @@ def single_product(request,id):
 
 
 def add_to_cart(request,id):
-    return render(request, 'add_to_cart')
+    return redirect('product')
+
+
+# def add_product(request):
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('product')
+#     else:
+#         form = ProductForm()
+#     return render(request, 'add_product.html',{'form':form})
+
+def add_product(request):
+    if request.method == 'POST':
+        product_title = request.POST.get('title','')
+        product_description = request.POST.get('description','')
+        product_price = request.POST.get('price','')
+        product_image = request.POST.get('image','')
+        product_category = request.POST.get('category','')
+
+        Product.objects.create(title=product_title,description=product_description,price=product_price,image=product_image,category_id=product_category)
+
+        return redirect('add_product')
+    return render(request, 'add_product.html')
